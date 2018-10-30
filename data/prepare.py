1df67e8e6dda797e84861f59944a8a1c2e7acb75
xmzhang <xinmingmail@163.com>
2018-10-31 06:51:32 +0800

    1031

--- data/prepare.py
+++ data/prepare.py
@@ -13,16 +13,16 @@ pyglet.font.load('ChloesMusic')
 
 SOUND = {}
 
-SOUND['d4'] = pyglet.resource.media('sound/D4.wav',streaming=False)
-SOUND['e4'] = pyglet.resource.media('sound/E4.wav',streaming=False)
-SOUND['f4'] = pyglet.resource.media('sound/F4.wav',streaming=False)
-SOUND['g4'] = pyglet.resource.media('sound/G4.wav',streaming=False)
-SOUND['a4'] = pyglet.resource.media('sound/A4.wav',streaming=False)
-SOUND['b4'] = pyglet.resource.media('sound/B4.wav',streaming=False)
-SOUND['c5'] = pyglet.resource.media('sound/C5.wav',streaming=False)
-SOUND['d5'] = pyglet.resource.media('sound/D5.wav',streaming=False)
-SOUND['e5'] = pyglet.resource.media('sound/E5.wav',streaming=False)
-SOUND['f5'] = pyglet.resource.media('sound/F5.wav',streaming=False)
+SOUND['d4'] = pyglet.resource.media('sound/d4.wav',streaming=False)
+SOUND['e4'] = pyglet.resource.media('sound/e4.wav',streaming=False)
+SOUND['f4'] = pyglet.resource.media('sound/f4.wav',streaming=False)
+SOUND['g4'] = pyglet.resource.media('sound/g4.wav',streaming=False)
+SOUND['a4'] = pyglet.resource.media('sound/a4.wav',streaming=False)
+SOUND['b4'] = pyglet.resource.media('sound/b4.wav',streaming=False)
+SOUND['c5'] = pyglet.resource.media('sound/c5.wav',streaming=False)
+SOUND['d5'] = pyglet.resource.media('sound/d5.wav',streaming=False)
+SOUND['e5'] = pyglet.resource.media('sound/e5.wav',streaming=False)
+SOUND['f5'] = pyglet.resource.media('sound/f5.wav',streaming=False)
 SOUND['laughter'] = pyglet.resource.media('sound/laughter.wav',streaming=False)
 
 MUSIC = '535353124325535353124321224431524325535353124321'
